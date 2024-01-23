2080dfca274a4d78ce3917f933edf6651689ac9c
Tatsuya Sakamoto <tatsuya.sakamoto@jmty.jp>
2024-01-23 14:15:57 +0900

    60%の位置に来た時に追加読み込み

--- app/src/main/java/com/example/infinite_scroll/MainActivity.kt
+++ app/src/main/java/com/example/infinite_scroll/MainActivity.kt
@@ -3,11 +3,13 @@ package com.example.infinite_scroll
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.OnScrollListener
 import com.example.infinite_scroll.databinding.ActivityMainBinding
 import com.example.infinite_scroll.databinding.RowArticleItemBinding
 
@@ -15,7 +17,7 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val articles = (0..30).map { it.toString() }
+    private val articles = (0..30).map { it.toString() }.toMutableList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,11 +32,34 @@ class MainActivity : AppCompatActivity() {
         binding.infiniteRecyclerView.layoutManager = layoutManager
         val adapter = ArticleAdapter(articles, this)
         binding.infiniteRecyclerView.adapter = adapter
+
+        binding.infiniteRecyclerView.addOnScrollListener(
+            object : OnScrollListener() {
+                override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    val extent = binding.infiniteRecyclerView.computeVerticalScrollExtent().toDouble()
+                    val offset = binding.infiniteRecyclerView.computeVerticalScrollOffset().toDouble()
+                    val range = binding.infiniteRecyclerView.computeVerticalScrollRange().toDouble()
+                    val total = range - extent
+                    val wariai = ((offset / total) * 100).toInt()
+
+                    Log.d("Tatsuya ٩( ᐛ )و", "現在のスクロール位置の割合: $wariai")
+
+                    if (wariai >= 60) {
+                        Log.d("Tatsuya ٩( ᐛ )و", "リストを追加！！　現在のスクロール位置の割合: $wariai")
+                        articles.addAll(articles)
+                        val adapter = (binding.infiniteRecyclerView.adapter as ArticleAdapter)
+                        adapter.addArticles(articles)
+                        adapter.notifyDataSetChanged()
+                    }
+                }
+            }
+        )
     }
 }
 
 class ArticleAdapter(
-    private val dataSet: List<String>,
+    private var dataSet: List<String>,
     private val context: Context,
 ) : RecyclerView.Adapter<ArticleAdapter.ViewHolder>() {
 
@@ -60,4 +85,7 @@ class ArticleAdapter(
         }
     }
 
+    fun addArticles(articles: List<String>) {
+        dataSet = articles
+    }
 }
