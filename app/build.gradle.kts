47ba590c33da4101085cc1b744b7e93989ddc9e6
Tatsuya Sakamoto <tatsuya.sakamoto@jmty.jp>
2024-01-23 12:37:19 +0900

    recyclerview使ってアイテム表示まで

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 
 dependencies {
 
+    implementation ("androidx.recyclerview:recyclerview:1.2.1")
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
--- app/src/main/java/com/example/infinite_scroll/MainActivity.kt
+++ app/src/main/java/com/example/infinite_scroll/MainActivity.kt
@@ -1,16 +1,63 @@
 package com.example.infinite_scroll
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.infinite_scroll.databinding.ActivityMainBinding
+import com.example.infinite_scroll.databinding.RowArticleItemBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val articles = (0..30).map { it.toString() }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setUpRecyclerView()
     }
+
+    private fun setUpRecyclerView() {
+        val layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        binding.infiniteRecyclerView.layoutManager = layoutManager
+        val adapter = ArticleAdapter(articles, this)
+        binding.infiniteRecyclerView.adapter = adapter
+    }
+}
+
+class ArticleAdapter(
+    private val dataSet: List<String>,
+    private val context: Context,
+) : RecyclerView.Adapter<ArticleAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val inflater: LayoutInflater = LayoutInflater.from(context)
+        val view = inflater.inflate(R.layout.row_article_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.set(dataSet[position])
+    }
+
+    override fun getItemCount(): Int {
+        return dataSet.size
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val bind: RowArticleItemBinding = RowArticleItemBinding.bind(itemView)
+
+        fun set(title: String) {
+            bind.textView.text = title
+        }
+    }
+
 }
